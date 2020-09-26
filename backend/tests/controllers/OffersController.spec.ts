93f72e6a20220d1bdbf10a609af378c347432396
Ghyslain Bruno <ghyslainbruno@gmail.com>
2020-09-27 01:38:41 +0200

    feat(offers unit test): add one simple unit test for the search offers feature - time to sleep

--- backend/tests/controllers/OffersController.spec.ts
+++ backend/tests/controllers/OffersController.spec.ts
@@ -1,5 +1,11 @@
+import Authentication from "../../api/models/Authentication";
+import Offer from "../../api/models/Offer";
+
 describe('Offers tests', () => {
   it('should search for offers', async () => {
-
+    jest.setTimeout(30000);
+    const user = await Authentication.signIn('test@gmail.com', '+33667182298');
+    const offers = await Offer.searchOffers(user.userToken, 48.870377, 2.370615, 48.882719, 2.322451);
+    expect(offers.length).toBeGreaterThan(1);
   });
 })
