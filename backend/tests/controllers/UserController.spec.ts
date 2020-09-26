de3663715ff75cd03a11ff1d7b731e9d2ec478e9
Ghyslain Bruno <ghyslainbruno@gmail.com>
2020-09-26 22:47:41 +0200

    feat(unit tests): unit test sign up feature

--- backend/tests/controllers/UserController.spec.ts
+++ backend/tests/controllers/UserController.spec.ts
@@ -2,7 +2,7 @@ import User from "../../api/models/User";
 import Authentication from "../../api/models/Authentication";
 
 describe('Authentication tests', () => {
-  it('should sign up the user', () => {
+  it('should sign up the user', async () => {
     const user = new User(
       'test@gmail.com',
       'Kim',
@@ -13,9 +13,29 @@ describe('Authentication tests', () => {
       'France'
     )
 
+    //TODO: we should mock the rp function and check that it is called with the proper parameters
+    // --> don't have the time to do such a thing for now
+    expect(user.userToken).toBeUndefined();
     expect(Authentication.doesUserExist).toBe(false);
-    Authentication.signUp(user);
+    await Authentication.signUp(user);
+    expect(user.userToken).toBeDefined()
     expect(Authentication.doesUserExist).toBe(true);
 
+  });
+
+  it('should sign in the user', async () => {
+    const user = new User(
+      'test@gmail.com',
+      'Kim',
+      'Hernandez',
+      '1996-05-17',
+      '+33667182298',
+      true,
+      'France'
+    )
+
+    expect(Authentication.isUserSignedIn).toBe(false);
+    await Authentication.signIn(user);
+    expect(Authentication.isUserSignedIn).toBe(true);
   });
 })
