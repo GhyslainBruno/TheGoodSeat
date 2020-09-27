39dd29f7629c1156df2f76d5f6b86a8eff68899d
Ghyslain Bruno <ghyslainbruno@gmail.com>
2020-09-27 20:06:38 +0200

    feat(build): add frontend build to repo - bad idea, but could be useful for testers

---
+++ backend/assets/precache-manifest.14b0b3884bd918f2eed225cffe8d9254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7368c92bd2db21baf89adde40ee212e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db452e854743d7ca3487",
+    "url": "/static/css/main.e500a6f7.chunk.css"
+  },
+  {
+    "revision": "7220b6020403b432476c",
+    "url": "/static/js/2.af23e6c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.af23e6c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db452e854743d7ca3487",
+    "url": "/static/js/main.e34068b8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
