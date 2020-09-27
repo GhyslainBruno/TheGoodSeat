c5facb1355b45f032b287d9ca0fcc326fe6c9b6f
Ghyslain Bruno <ghyslainbruno@gmail.com>
2020-09-27 14:04:11 +0200

    feat(frontend): use ky library (from sindresorhus - big fan of his work) to make HTTP calls in the browser

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './App';
 import { Provider } from "react-redux";
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
+// @ts-ignore
+window.store = store;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
   document.getElementById('root')
 );
 
